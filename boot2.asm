1312287ce6cf8047d5a1f8920c0d400da99c3405
Ryan de Jonge <naamloosdt@gmail.com>
2020-03-09 15:17:41 +0100

    input???????????

--- boot2.asm
+++ boot2.asm
@@ -3,12 +3,28 @@ org 0x7c00
 boot:
 	mov si, hello
 	mov ah,0x0e
+    mov ah, 00h
+    int 16h
+    cmp al, 49
+    je .opt1
+    cmp al, 50
+    je .opt2
+
 .loop:
 	lodsb
 	or al, al
 	jz halt
 	int 0x10
 	jmp .loop
+
+.opt1:
+    db "you press 1"
+    jmp .loop
+ 
+.opt2:
+    db "2 breh"
+    jmp .loop
+
 halt:
 	cli
 	hlt
