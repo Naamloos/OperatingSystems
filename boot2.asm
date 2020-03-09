cf2793950e7552534a8c5510f8255c92c3e2a899
Ryan de Jonge <naamloosdt@gmail.com>
2020-03-09 15:28:34 +0100

    what the actual f-

--- boot2.asm
+++ boot2.asm
@@ -1,33 +1,40 @@
 bits 16
 org 0x7c00
 boot:
-	mov si, hello
-	mov ah,0x0e
-    mov ah, 00h
-    int 16h
-    cmp al, 49
-    je .opt1
-    cmp al, 50
-    je .opt2
+	mov si, hello       ; show welcome msg
+	mov ah,0x0e         ; Write character in titty mode
 
-.loop:
-	lodsb
-	or al, al
-	jz halt
-	int 0x10
-	jmp .loop
+    .loop:              ; this is our loop
+        lodsb
+        or al, al       ;|
+        jz input        ;| jump to input if al is zero
 
-.opt1:
-    db "you press 1",13,10
-    jmp .loop
- 
-.opt2:
-    db "2 breh",13,10
-    jmp .loop
+        cmp al, 49      ;|
+        je .opt1        ;|
 
-halt:
-	cli
-	hlt
+        cmp al, 50
+        je .opt2
+
+        int 0x10
+        jmp .loop
+
+    .opt1:
+        db "you press 1",13,10
+        jmp .loop
+    
+    .opt2:
+        db "2 breh",13,10
+        jmp .loop
+
+input:              ;|
+    mov ah, 00h     ;|
+    int 16h         ;| Getting input uhh
+    jmp .loop       ; Jumping back to loop
+
+halt:               ;|
+	cli             ;|
+	hlt             ;| Halting program
+
 hello: 
 	db "Welcome to TempleOS",13,10
 
