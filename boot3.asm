dd12b6d10729fbb39ffc1853e5bf085e0f9b1425
thijsrijkers <gamearea20@gmail.com>
2020-03-18 12:30:44 +0100

    Tekst, Tekst?! TEKST?!x

--- boot3.asm
+++ boot3.asm
@@ -46,4 +46,18 @@ boot3:
     mov fs, ax
     mov gs, ax
     mov ss, ax
-    
+
+    mov esi , hello
+    mov ebx, 0xb8000
+.loop:
+    lodsb
+    or al,al
+    jz halt
+    or eax, 0x0100
+    mov word [ebx], ax
+    add ebx,2
+    jmp .loop
+halt: 
+    cli
+    hlt
+hello: db "Goeie dag eem!",0
